1f28c2dbb9f8de196e37e85418f2a73d182a325b
Penar Musaraj <pmusaraj@gmail.com>
2019-02-27 11:14:50 -0500

    preseed experiements

--- script/docker_preseed.rb
+++ script/docker_preseed.rb
@@ -12,47 +12,32 @@ def run_or_fail(command)
   exit 1 unless $?.exitstatus == 0
 end
 
-unless ENV['NO_UPDATE']
-  puts "travis_fold:start:pulling_latest_discourse" if ENV["TRAVIS"]
-  run_or_fail("git reset --hard")
-  run_or_fail("git pull")
+#  if ENV['PARALLEL_TESTS']
+@parallel_count = ENV["PARALLEL_COUNT"] || 3
 
-  checkout = ENV['COMMIT_HASH'] || "HEAD"
-  run_or_fail("git checkout #{checkout}")
-  puts "travis_fold:end:pulling_latest_discourse" if ENV["TRAVIS"]
+puts "travis_fold:start:prepare_tests" if ENV["TRAVIS"]
+puts "Cleaning up old test tmp data in tmp/test_data"
+`rm -fr tmp/test_data && mkdir -p tmp/test_data/redis && mkdir tmp/test_data/pg`
 
-  puts "travis_fold:start:bundle" if ENV["TRAVIS"]
-  run_or_fail("bundle")
-  puts "travis_fold:end:bundle" if ENV["TRAVIS"]
+puts "Starting background redis"
+@redis_pid = Process.spawn('redis-server --dir tmp/test_data/redis')
 
-  if ENV['PARALLEL_TESTS']
-    @parallel_count = ENV["PARALLEL_COUNT"] || 1
+@postgres_bin = "/usr/lib/postgresql/10/bin/"
+`#{@postgres_bin}initdb -D tmp/test_data/pg`
 
-    puts "travis_fold:start:prepare_tests" if ENV["TRAVIS"]
-    puts "Cleaning up old test tmp data in tmp/test_data"
-    `rm -fr tmp/test_data && mkdir -p tmp/test_data/redis && mkdir tmp/test_data/pg`
+# speed up db, never do this in production mmmmk
+`echo fsync = off >> tmp/test_data/pg/postgresql.conf`
+`echo full_page_writes = off >> tmp/test_data/pg/postgresql.conf`
+`echo shared_buffers = 500MB >> tmp/test_data/pg/postgresql.conf`
 
-    puts "Starting background redis"
-    @redis_pid = Process.spawn('redis-server --dir tmp/test_data/redis')
+puts "Starting postgres"
+@pg_pid = Process.spawn("#{@postgres_bin}postmaster -D tmp/test_data/pg")
 
-    @postgres_bin = "/usr/lib/postgresql/10/bin/"
-    `#{@postgres_bin}initdb -D tmp/test_data/pg`
+ENV["RAILS_ENV"] = "test"
 
-    # speed up db, never do this in production mmmmk
-    `echo fsync = off >> tmp/test_data/pg/postgresql.conf`
-    `echo full_page_writes = off >> tmp/test_data/pg/postgresql.conf`
-    `echo shared_buffers = 500MB >> tmp/test_data/pg/postgresql.conf`
-
-    puts "Starting postgres"
-    @pg_pid = Process.spawn("#{@postgres_bin}postmaster -D tmp/test_data/pg")
-
-    ENV["RAILS_ENV"] = "test"
-
-    @good &&= run_or_fail("bundle exec rake parallel:create[#{@parallel_count}]")
-    @good &&= run_or_fail("bundle exec rake parallel:migrate[#{@parallel_count}]")
-    puts "bundle exec rake parallel:spec STARTED with #{@parallel_count} parallel tests"
-    @good &&= run_or_fail("bundle exec rake parallel:spec[#{@parallel_count}]")
-    puts "bundle exec rake parallel:spec ENDED"
-  end
-
-end
+@good &&= run_or_fail("bundle exec rake parallel:create[#{@parallel_count}]")
+@good &&= run_or_fail("bundle exec rake parallel:migrate[#{@parallel_count}]")
+puts "bundle exec rake parallel:spec STARTED with #{@parallel_count} parallel tests"
+@good &&= run_or_fail("bundle exec rake parallel:spec[#{@parallel_count}]")
+puts "bundle exec rake parallel:spec ENDED"
+#  end
